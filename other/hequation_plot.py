16e7d977cadadb3960937350f8dfe0ac998634a2
Margherita <mtonon.ieu2023@student.ie.edu>
2025-05-16 15:35:51 +0200

    created script to make a plot for report

---
+++ other/hequation_plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_array = np.arange(-200, 200, 0.1)
+
+def h_function(arr):
+    return (0.1 * np.arctan(arr))/np.arctan(2)
+
+h_array = h_function(x_array)
+
+plt.plot(x_array, h_array)
+plt.hlines(0, ls="--", color = "gray", xmin = -205, xmax = 205)
+plt.title("Plot of h(x̂)", fontsize = 30)
+plt.grid()
+plt.show()
